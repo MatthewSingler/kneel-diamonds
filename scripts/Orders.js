f1c2768387dfcecfab77e29df509399a1c73454b
Matthew Singler <matthew.singler@gmail.com>
2021-07-29 15:51:01 -0500

    Tried adding total cost and completely wrecked everyting. Need to define order, but have no idea how to do that

--- scripts/Orders.js
+++ scripts/Orders.js
@@ -1,4 +1,4 @@
-import { getOrders } from "./database.js"
+import { getOrders, getMetals} from "./database.js"
 
 const buildOrderListItem = (order) => {
     return `<li>
@@ -9,16 +9,41 @@ const buildOrderListItem = (order) => {
 export const Orders = () => {
     
     /*
-        Can you explain why the state variable has to be inside
-        the component function for Orders, but not the others?
+    Can you explain why the state variable has to be inside
+    the component function for Orders, but not the others?
     */
-    const orders = getOrders()
+   const orders = getOrders()
+    const metals = getMetals()
+    
+    const orderArray = (orderObj) => {
+        for (const orderObj of ) {
+           return 
+       } 
+    }
+    orderArray(order)
+
+   // Remember that the function you pass to find() must return true/false
+   const foundMetal = metals.find(
+       (metal) => {
+           return metal.id === order.metalId
+       }
+   )
+   const totalCost = foundMetal.price
+   const costString = totalCost.toLocaleString("en-US", {
+       style: "currency",
+       currency: "USD"
+   })
+   
+       `<li>
+       Order #${orders.id} cost ${costString}
+   </li>`
 
     let html = "<ul>"
 
     const listItems = orders.map(buildOrderListItem)
 
     html += listItems.join("")
+    
     html += "</ul>"
 
     return html
